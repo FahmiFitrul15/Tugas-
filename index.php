d08a717eef0267e6ff364983d1fe6a0c1b103adf
Fahmi Fitrul <100229632+FahmiFitrul15@users.noreply.github.com>
2022-02-23 01:43:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>web html css</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="navbar">
+			<ul> 
+				<li class="active"><a href="#">HOME</a></li>
+				<li><a href="#">DESAIN GRAPIS</a></li>
+				<li><a href="#">VIDEOGRAPI</a></li>
+				<li><a href="#">TENTANG</a></li>
+				<li><a href="#">LOGOUT</a></li>
+			</ul>
+			<div class="contents">
+			</div>
+
+</body>
+</html>
